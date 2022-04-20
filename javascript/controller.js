c9e50a603650b4a2308662c1f7da5010fc9902e5
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2022-04-20 12:59:22 +0530

    4.2 Updated

---
+++ javascript/controller.js
@@ -0,0 +1,20 @@
+// Right Click Blocker 
+// document.addEventListener('contextmenu', (AnkanSaha)=>{
+//     AnkanSaha.preventDefault();
+// })
+
+// Interval Settings 
+setInterval(DateGenerator(), 100)
+
+// Functions 
+function DateGenerator() {
+    let ctime = document.getElementById("timer");
+    let current = new Date();
+    let TodayDate = current.getDate()
+    let Month = current.getMonth()
+    let Year = current.getFullYear()
+    let FinalDate = "Today Date is "+" :- "+TodayDate+" - "+Month+" - "+Year
+
+    // Sending Date To HTML Document 
+    ctime.innerText = FinalDate
+}
---
+++ javascript/projectsignin.js
@@ -0,0 +1,101 @@
+// Access Key Array 
+let Keys = ["458756328", "457845124", "784515364", "784512365", "789562354", "451235647","784535267", "986532154", "999944441", "244466666", "784697164", "253689459", "736745982"]
+
+// Manual Signin Methods 
+document.getElementById('Loginbtn').addEventListener('click', ()=>{
+    let tempuserkey = document.getElementById('AccessCode').value
+    if(tempuserkey.length > 0 && tempuserkey.length < 10){
+        Keys.forEach((e)=>{
+            if(tempuserkey == e){
+                alert("User Authenticated");
+                let fgre = document.getElementById("signins")
+                let projectWindow = document.getElementById("projects")
+                projectWindow.style.display = "block"
+                console.log(fgre)
+                localStorage.setItem('Access Code', e)
+                fgre.style.display = "none"
+            }
+        })
+    }
+    else if(tempuserkey.length > 9){
+        alert("Access Key is So Big, please Enter valid 9 digit key")
+    }
+    else{
+        alert("Please Enter valid 9 Digit Access key")
+    }
+})
+
+// Auto Signin Method 
+document.getElementById('AutoLogins').addEventListener('click', ()=>{
+    let tempsavedkey = localStorage.getItem("Access Code");
+    console.log(tempsavedkey)
+    if(tempsavedkey == null){
+        alert("You Don't Have any Saved Access Key, Please Enter Valid Access key");
+    }
+    else if(tempsavedkey == ""){
+        alert("You Don't Have any Saved Access Key, Please Enter Valid Access key");
+    }
+    else if(tempsavedkey != null){
+        if(tempsavedkey.length > 0 && tempsavedkey.length < 10){
+            Keys.forEach((e)=>{
+                if(tempsavedkey == e){
+                    alert("User Authenticated");
+                    let fgre = document.getElementById("signins")
+                    console.log(fgre)
+                    localStorage.setItem('Access Code', e)
+                    fgre.style.display = "none"
+                    let projectWindow = document.getElementById("projects")
+                    projectWindow.style.display = "block"
+                }
+            })
+        }
+        else if(tempsavedkey.length > 9){
+            alert("Access Key is So Big, please Enter valid 9 digit key")
+        }
+        else{
+            alert("Please Enter valid 9 Digit Access key")
+        }
+    }
+    else if(tempsavedkey != ""){
+        if(tempsavedkey.length > 0 && tempsavedkey.length < 10){
+            Keys.forEach((e)=>{
+                if(tempsavedkey == e){
+                    alert("User Authenticated");
+                    let fgre = document.getElementById("signins")
+                    console.log(fgre)
+                    localStorage.setItem('Access Code', e)
+                    fgre.style.display = "none"
+                    let projectWindow = document.getElementById("projects")
+                    projectWindow.style.display = "block"
+                }
+            })
+        }
+        else if(tempsavedkey.length > 9){
+            alert("Access Key is So Big, please Enter valid 9 digit key")
+        }
+        else{
+            alert("Please Enter valid 9 Digit Access key")
+        }
+    }
+})
+
+// Auto Saved Login Button Shower 
+function AutoLoginsShoower(){
+    let temporarykeyfatch = localStorage.getItem("Access Code");
+    if(temporarykeyfatch == null){
+        document.getElementById("AutoLogins").style.display = "none"
+    }
+    else if(temporarykeyfatch == ""){
+        document.getElementById("AutoLogins").style.display = "none"
+    }
+    else if(temporarykeyfatch != null){
+        document.getElementById("AutoLogins").style.display = ""
+    }
+    else if(temporarykeyfatch != ""){
+        document.getElementById("AutoLogins").style.display = ""
+    }
+}
+
+// Run Auto Saved Login Button Shower function 
+AutoLoginsShoower()
+
