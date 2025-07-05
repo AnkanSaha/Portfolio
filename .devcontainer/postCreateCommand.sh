d7bab6237ef4a5a92a7eab5d06a6596e2740ce55
Ankan Saha <ankansahadaluabari@gmail.com>
2025-07-05 23:06:14 +0000

    feat: add devcontainer and Gitpod configuration files, update backend package version to 0.0.2 with MIT license

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update -y           # Update apt-get cache
+sudo apt-get install -y neofetch # Install curl
+
+# install Node.js and npm
+curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash
+sudo apt-get install -y nodejs # Install Node.js and npm
+
+# Install Dependencies for frontend
+cd frontend && npm i --force && npm run build && cd ../
+
+# Install Dependencies for backend
+cd backend && npm i --force && npm run build && cd ../
